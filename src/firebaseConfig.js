51a48d5e7e51a2fd6d1d3eebe42759c634348f1c
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-12-06 09:32:29 +0100

    deleted unnecessary files and added firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyBpl-bhHeCVRfBUZ948LS-qpjGkOPDfCI8",
+    authDomain: "poniedzialek-26.firebaseapp.com",
+    databaseURL: "https://poniedzialek-26.firebaseio.com",
+    projectId: "poniedzialek-26",
+    storageBucket: "poniedzialek-26.appspot.com",
+    messagingSenderId: "77751688488"
+}
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth()
+export const database = firebase.database()
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
